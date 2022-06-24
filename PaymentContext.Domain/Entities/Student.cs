45d8d9c42a1f86a633874ce13336b80912b6d5a1
Dharuan Emina <dharuanluigi@gmail.com>
2022-06-24 12:09:48 -0300

    Added: modeling entities

---
+++ PaymentContext.Domain/Entities/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace PaymentContext.Domain.Entities
+{
+  public class Student
+  {
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string Document { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public IList<Subscription> Subscriptions { get; set; } = null!;
+  }
+}
---
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -0,0 +1,15 @@
+﻿namespace PaymentContext.Domain.Entities
+{
+  public class Subscription
+  {
+    public DateTime CreatedDate { get; set; }
+
+    public DateTime UpdatedDate { get; set; }
+
+    public DateTime? ExpireDate { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public IList<Payment> Payments { get; set; }
+  }
+}
